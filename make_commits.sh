9a4154a51ea8fe209464317178f0e05de62dee68
Søren Fuglede Jørgensen <s@fuglede.dk>
2016-04-02 16:44:11 +0200

    Add commit script

--- make_commits.sh
+++ make_commits.sh
@@ -1,4 +1,9 @@
-# Make some random changes
-for i in {10..1000}; do
+# For each date in the output of make_dates.go, make a random change
+# and commit it at that date.
+dates=$(go run make_dates.go)
+while read -r date; do
+	echo "Making a commit at $date"
 	echo $RANDOM > trash
-done
+	GIT_AUTHOR_DATE=$date GIT_COMMITTER_DATE=$date
+	git commit -m "Adding trash to trash" trash > /dev/null
+done <<< "$dates"
